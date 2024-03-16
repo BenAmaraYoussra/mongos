accb4d8bc281bba121b569a7f0d28f1fa34eef20
BenAmaraYoussra <benamarayousra3@gmail.com>
2024-03-16 21:49:18 +0100

    seconde commit

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-//1 require express
-const express = require('express')
+//le serveur
+//importation et creation de linstance express
+const express= require('express');
+const app=express();
 
-//2 instance from express
-const app = express()
-//5 require dotenv
-require('dotenv').config()
+//importation du dotenv qui contient le lien de DB et le port
+require("dotenv").config();
 
-//6 connect to db
-const connectDB = require('./config/connectDB')
-connectDB()
+// midelware qui traduit les fichier json
+app.use(express.json());
 
-//3 PORT 
+//connection a la DB grace au fichier connectDB
+const connectDB= require('./config/connectDB')
+connectDB();
+
+//le routeur
+app.use('/api/contact',require('./Routes/contact'));
+
+// lancer le serveur dans le port indiqué
 const PORT = process.env.PORT
+app.listen(PORT,(err)=>{
+    err? console.log(err):
+    console.log(`Le  serveur fonctionne sur le port:http://localhost:${PORT}`);
 
-//4 server
-app.listen(PORT, (err)=>{
-    err?
-    console.log(err):
-    console.log(`the server is running https://localhost:${PORT}`);
 
-})
+});      
